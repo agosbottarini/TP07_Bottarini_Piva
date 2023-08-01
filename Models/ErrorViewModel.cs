f8c813883d54f5351841c73d03380083513f398d
AGOSTINA BOTTARINI <47866440@ort>
2023-08-01 11:43:50 -0300

    Subiendo proyecto base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP07_Bottarini_Piva.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
